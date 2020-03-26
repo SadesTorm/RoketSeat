d5717449df0c5effd15ae85f1beff34acafcbac1
Paulo Henrique Alves <xxpaulinho.xx@live.com>
2020-03-26 11:50:42 -0300

    Front main + css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-
+import './styleGlobal.css';
 import Header from './Header';
 import Logon from './pages/Logon';
 
 
+
 function App() {
 
-  //OBS: Compontente sempre com letra Maiuscula no inicio
-  // componente é uma função que restorna um HTML
-  // liveReload -> troca valor da page em tempo real 
-  //JSX - quando o html esta integrado dentro do Js
 
-  const [contador,setContador] = useState(0);
-  
-  //retorna array [valor variavel, função de att do valor]
-
-  function incremento(){
-    setContador(contador + 1);
-    console.log(contador)
-  }
-  
   return (
     <div>
       
-      < Header> 
-      contador: {contador}
-      </Header>  
-      <button onClick={incremento}>Increment</button>
+     <Logon />
+    
       
     </div>
   
@@ -37,3 +23,29 @@ function App() {
 }
 
 export default App;
+  //OBS: Compontente sempre com letra Maiuscula no inicio
+  // componente é uma função que restorna um HTML
+  // liveReload -> troca valor da page em tempo real 
+  //JSX - quando o html esta integrado dentro do Js
+  //-------------------------------------------------------------------------
+  // const [contador,setContador] = useState(0);
+  
+  //retorna array [valor variavel, função de att do valor]
+  /*
+  function incremento(){
+    setContador(contador + 1);
+    console.log(contador)
+  }*/
+
+  /*
+  <div>
+      
+      < Header> 
+      contador: {contador}
+      </Header>  
+      <button onClick={incremento}>Increment</button>
+      
+    </div>
+  */
+
+  //-------------------------------------------------------------------------------
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import {FiLogIn} from 'react-icons/fi';
 import './styles.css'
+import  imageHero from '../../assets/heroes.png'
+import  imageLogo from '../../assets/logo.svg'
 
 export default function Logon(){
     return(
-        <h1> tesntando index login </h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={imageLogo} alt="Be - The - Hero"/>
+
+                    <form>
+                        <h1>Faça seu Longon</h1>
+
+                        <input placeholder="Sua ID"/>
+                        <button type="summit"> Entrar </button>
+                        <a href="/register">
+                            <FiLogIn size={16} color="#E02041" />
+                            Cadastre-se
+                        </a>
+
+                    </form>
+                 </section>
+                 <img src={imageHero} alt="HEROES"/>
+        </div>
+        
     );
 }
