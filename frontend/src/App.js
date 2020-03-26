364be1bcded42879fd3e5ef105b6e960ec3a5fca
Paulo Henrique Alves <xxpaulinho.xx@live.com>
2020-03-26 09:19:22 -0300

    front

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+import Header from './Header';
+
+function App() {
+
+  //OBS: Compontente sempre com letra Maiuscula no inicio
+  // componente é uma função que restorna um HTML
+  // liveReload -> troca valor da page em tempo real 
+  //JSX - quando o html esta integrado dentro do Js
+  
+  return (
+    <Header> 
+      Testando passagem de valor por paramentro
+    </Header>  
+    
+  );
+}
+
+export default App;
